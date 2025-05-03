fbc2d7dbe217ed7133cecdef79c22f7860ec521c
Alex Olson <alexko22222@gmail.com>
2025-05-02 22:35:02 -0400

    updating repo

---
+++ assign1.py
@@ -0,0 +1,22 @@
+
+# Task 1 (basic variable creation)
+name = "Alex"
+age = "22"
+height = 6.0
+
+# Task 2 (operations)
+num1 = 22
+num2 = 99
+print("The sum of 22 and 99 is", num1 + num2)
+print("The difference of 22 and 99 is", num1 - num2)
+print("The product of 22 and 99 is", num1 * num2)
+print("The quotient of 22 and 99 is", num1 / num2)
+
+# Task 3 (number checker)
+numX = input("Enter a number: ")
+if (numX > 0):
+    print("The number is positive. Awesome!")
+elif (numX < 0):
+    print("The number is negative. Better luck next time!")
+elif (num == 0):
+    print("Zero is it. A perfect balance!")
